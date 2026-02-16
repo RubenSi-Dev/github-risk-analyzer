b45fbcbb4a9ab8d04d4673f67c28656d58e4d3d3
Ruben Simons <ruben.simons@student.kuleuven.be>
2026-02-16 13:09:03 +0100

    authentication with .env file

--- internal/authentication/client.go
+++ internal/authentication/client.go
@@ -3,19 +3,28 @@ package authentication
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/google/go-github/github"
+	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 )
 
 func GetAuthorizedClient(ctx context.Context) (*github.Client, error) {
-	token, err := ioutil.ReadFile("../../pat.txt")
+	// Load .env file if it exists
+	err := godotenv.Load()
 	if err != nil {
-		return nil, fmt.Errorf("failed parsing token-file path: %v", err)
+		return nil, fmt.Errorf("Error loading .env file: %w", err)
 	}
+	token := os.Getenv("GITHUB_TOKEN")
+	token = strings.TrimSpace(token)
 
+	if token == "" {
+		return nil, fmt.Errorf("GITHUB_TOKEN environment variable is not set")
+	}
+	//token, err := ioutil.ReadFile("../../pat.txt")
 	var httpClient *http.Client
 
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: string(token)})
