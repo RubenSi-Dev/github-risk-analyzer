223e5a48ace96e8d788b35cced4aa91e98e04c11
Ruben Simons <ruben.simons@student.kuleuven.be>
2026-02-21 15:27:04 +0100

    basic functionality for go support

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require github.com/google/go-github v17.0.0+incompatible
 require (
 	github.com/google/go-querystring v1.2.0 // indirect
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/mod v0.33.0
 	golang.org/x/oauth2 v0.35.0 // direct
 )
--- internal/parser/depsGo.go
+++ internal/parser/depsGo.go
@@ -1,1 +1,12 @@
 package parser
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+	"github.com/rubensi-dev/github-risk-analyzer/internal/models"
+)
+
+func ExtractDepsGo(ctx context.Context, repo models.Repository, client *github.Client) ([]models.Dependency, error) {
+
+}
